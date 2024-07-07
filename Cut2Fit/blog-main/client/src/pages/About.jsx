786748a4425ad730d3a9cf9b1d67283a6302c3bd
Ilma Ilmy <ifilma2001@gmail.com>
2024-07-07 19:52:39 +0530

    Blog page added.

---
+++ Cut2Fit/blog-main/client/src/pages/About.jsx
@@ -0,0 +1,30 @@
+export default function About() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Sahand' Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+            Welcome to Cut2Fit! Many individuals struggle with finding clothes that fit perfectly, especially when it comes to customized dresses. The lack of accurate measurements and cutting patterns often results in ill-fitting garments, leading to dissatisfaction among customers. Cut2Fit aims to address this issue by providing users with a platform where they can input their dress model through drawing or customization. The platform will then calculate the actual measurements and generate cutting patterns, ensuring a perfect fit for tailored dresses.
+            </p>
+
+            <p>
+              On this blog, you'll find weekly articles and tutorials on topics
+              such as Fahion Trends, Apparel Industry, Technology and Clothing. Be sure to check back often for new content!
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
